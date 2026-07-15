8d6b9404b32b8393fb42350e4f6913d66f04c7eb
Your Name <you@example.com>
2026-07-15 11:46:58 -0300

    refactor: remove unused data unit conversion utility functions

--- src/conversion_utils.ts
+++ src/conversion_utils.ts
@@ -1,27 +1,3 @@
-export function gigabytesToMegabits(gigabytes: number): number {
-  return gigabytes * 8 / (1024 * 1024);
-}
-
-export function megabitsToGigabytes(megabits: number): number {
-  return megabits / (8 * 1024 * 1024);
-}
-
-export function kilobitsToMegabits(kilobits: number): number {
-  return kilobits * 8 / (1024);
-}
-
-export function bytesToMegabits(bytes: number): number {
-  return bytes * 8 / (1024);
-}
-
-export function gigabytesToKilobits(gigabytes: number): number {
-  return gigabytes * 8 * 1024 / (1024);
-}
-
-export function gigabytesToBits(gigabytes: number): number {
-  return gigabytes * 8 * 1024 * 1024 * 1024;
-}
-
 export function gigabytesToTerabits(gigabytes: number): number {
   return gigabytes * 8 / 1024;
 }
@@ -29,4 +5,3 @@ export function gigabytesToTerabits(gigabytes: number): number {
 export function terabitsToGigabytes(terabits: number): number {
   return terabits * 1024 / 8;
 }
-
