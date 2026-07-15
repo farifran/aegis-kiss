2017ec62337ddbf4a7a79dfd359308f014baf713
Your Name <you@example.com>
2026-07-15 12:20:44 -0300

    chore: remove unused conversion utility functions

--- src/conversion_utils.ts
+++
@@ -1,7 +0,0 @@
-export function gigabytesToTerabits(gigabytes: number): number {
-  return gigabytes * 8 / 1024;
-}
-
-export function terabitsToGigabytes(terabits: number): number {
-  return terabits * 1024 / 8;
-}
