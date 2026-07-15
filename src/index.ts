092a771b4e4e3b4c2ce958f0b2545ee840ffcdab
Your Name <you@example.com>
2026-07-15 19:20:52 -0300

    feat: add bitsToTerabits conversion utility function

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ export function quadratica(x: number): number {
   return x * x;
 }
 
+export function megabitsToKilobits(megabits: number): number {
+  return megabits * 1000 / 1024;
+}
+
 export function bitsToTerabits(bits: number): number {
   return bits / (1024 ** 4);
 }
