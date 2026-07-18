2fca47e9e41bc5faf00e2bf64f305fb69be463e6
Your Name <you@example.com>
2026-07-18 20:16:17 -0300

    refactor: remove unused data conversion utility functions

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,3 @@
-export function kilobytesToPentabits(kilobytes: number): number {
-  return kilobytes * 1024 * 1024 * 1024 * 1024;
-}
-
-export function kilobytesToBits(kilobytes: number): number {
-  return kilobytes * 1024;
-}
-
 export function bytesToBits(bytes: number): number {
   return bytes * 8;
-}
-
-export function bytesToKilobits(bytes: number): number {
-  return bytesToBits(bytes) / 1024;
-}
-
-export function terabitsToMegabits(terabits: number): number {
-  return terabits * 1024;
-}
-
-export function bytesToGigabits(bytes: number): number {
-  return bytesToBits(bytes) / (1024 * 1024 * 1024);
-}
-
-export function bytesToPentabits(bytes: number): number {
-  return bytesToBits(bytes) / (1024 * 1024 * 1024 * 1024);
-}
-
-export function megabytesToPentabits(megabytes: number): number {
-  return megabytes * 1024 * 1024 * 1024 * 1024;
-}
-
-export function gigabytesToPentabits(gigabytes: number): number {
-  return gigabytes * 1024 * 1024 * 1024 * 1024;
-}
-
-export function bitsToPentabits(bits: number): number {
-  return bits / (1024 * 1024 * 1024 * 1024);
 }
