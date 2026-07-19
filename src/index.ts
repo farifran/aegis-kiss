de6cffe0b83d298074f91de5582b4c665fa1a39b
Your Name <you@example.com>
2026-07-18 23:55:09 -0300

    refactor: replace kilobytesToBits function with kilobitsToGigabytes and update test metadata

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export function kilobytesToBits(kilobytes: number): number {
-    return kilobytes * 1024;
+export function kilobitsToGigabytes(kilobits: number): number {
+    return kilobits / 1024 / 1024;
 }
