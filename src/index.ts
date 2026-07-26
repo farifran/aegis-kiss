ba66a42f6f93c648a54173eebf0f127f195124b1
Your Name <you@example.com>
2026-07-25 22:13:50 -0300

    feat: add converterKilobitsEmBits function and update resonance metadata

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 export function converter(megabytes: number): number {
   return megabytes * 8000;
 }
+
+export function converterKilobitsEmBits(kilobits: number): number {
+  return kilobits * 1000;
+}
