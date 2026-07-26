986949475c49e1b6669e3ca5b831a0edd938bbb5
Your Name <you@example.com>
2026-07-25 21:55:16 -0300

    feat: add converterMegabyteEmKilobits function and update git hash reference

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-export function converter(kilobits: number): number {
-  // 1 Gigabyte = 8,388,608 Kilobits (using binary definitions: 1 GB = 1024^3 Bytes, 1 Kb = 1024 Bits)
-  return kilobits / 8388608;
-}
-
-export function converterGigabitsEmTerabits(gigabits: number): number {
-  // 1 Terabit = 1024 Gigabits (binary definition)
-  return gigabits / 1024;
-}
