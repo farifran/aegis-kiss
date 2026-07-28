7f9222764fd2cd8dd1a11ac245084d21db4f1d3d
Your Name <you@example.com>
2026-07-28 17:01:20 -0300

    feat: add converterSemanaEmSegundo helper function to index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,8 @@ export function converterBitsToKilobytes(bits: number): number {
   // 1 Kilobyte = 8000 bits
   return bits / 8000;
 }
+
+export function converterSemanaEmSegundo(semanas: number): number {
+  // 1 Semana = 604800 Segundos
+  return semanas * 604800;
+}
