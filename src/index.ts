72da9709ba4d8f57e2f8f051b6236f6d9cb37b97
Your Name <you@example.com>
2026-07-28 16:47:09 -0300

    feat: add converterBitsToKilobytes utility function

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,8 @@ export function converter(megabits: number): number {
   // 1 Megabit = 125 Kilobytes
   return megabits * 125;
 }
+
+export function converterBitsToKilobytes(bits: number): number {
+  // 1 Kilobyte = 8000 bits
+  return bits / 8000;
+}
