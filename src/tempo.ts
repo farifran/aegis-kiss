dcca442540e524c6905d09a757ad0affb580d340
Your Name <you@example.com>
2026-07-28 12:36:56 -0300

    feat: add converterSemanasEmSegundos utility function

--- src/tempo.ts
+++ src/tempo.ts
@@ -1,3 +1,7 @@
 export function converterSegundosEmMilissegundos(segundos: number): number {
   return segundos * 1000;
 }
+
+export function converterSemanasEmSegundos(semanas: number): number {
+  return semanas * 604800;
+}
