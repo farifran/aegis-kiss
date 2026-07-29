1be72fa8c9cd230eb906c23b2c08ea7f60664278
Your Name <you@example.com>
2026-07-29 00:04:13 -0300

    feat: complete L7 dijkstra graph module

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { avaliarExpressaoAritmetica } from './evaluator.js';
 export { rateLimiterSlidingWindow } from './rateLimiter.js';
 export { calcularCRC32 } from './crc32.js';
+export { dijkstraShortestPath } from './dijkstra.js';
