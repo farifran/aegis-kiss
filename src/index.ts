8d1e31fa614d88e731005c05292d0f1221c1d6b9
Your Name <you@example.com>
2026-07-29 08:26:10 -0300

    chore: cleanup benchmark test modules from src

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,1 @@
-export { avaliarExpressaoAritmetica } from './evaluator.js';
-export { rateLimiterSlidingWindow } from './rateLimiter.js';
-export { calcularCRC32 } from './crc32.js';
-export { dijkstraShortestPath } from './dijkstra.js';
-export { LRUCache } from './lruCache.js';
-export { AVLTree } from './avlTree.js';
-export {
-  SelectQueryBuilder,
-  InsertQueryBuilder,
-  UpdateQueryBuilder,
-  DeleteQueryBuilder,
-} from './queryBuilder.js';
-export { HTTPServer } from './httpServer.js';
-export type { HTTPRequest, HTTPResponse } from './httpServer.js';
-export { EventEmitter, getEventEmitter } from './eventEmitter.js';
+// Aegis workspace entry point
