f282943d90eea2c33d241cf399d63133843d051f
Your Name <you@example.com>
2026-07-29 09:21:29 -0300

    refactor: remove unused converterGigabitsEmTerabits function from entry point

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-// Aegis workspace entry point
-
-function converterGigabitsEmTerabits() {
-  // implementation of the converterGigabitsEmTerabits function
-  return BigInt(1) * BigInt(1e12);
-}
-
-export { converterGigabitsEmTerabits };
