7779c9e4ab38b170fd70271ab6dff763643626a5
Your Name <you@example.com>
2026-07-29 08:57:07 -0300

    aegis: issue#21 converterGigabitsEmTerabits
    Aegis-Issue: 21
    Aegis-Accept: converterGigabitsEmTerabits

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
 // Aegis workspace entry point
+
+function converterGigabitsEmTerabits() {
+  // implementation of the converterGigabitsEmTerabits function
+  return BigInt(1) * BigInt(1e12);
+}
+
+export { converterGigabitsEmTerabits };
