c7869bd5d763f55655d7baafa0cbfa2cddab137a
Your Name <you@example.com>
2026-07-30 10:15:20 -0300

    refactor: remove unused gigabits and kilobits conversion functions

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-export function converterGigabitsEmTerabits(gb: number): number {
-  return gb / 1000;
-}
-
-export function converterKilobitsEmTerabits(kb: number): number {
-  return kb / 1000;
-}
