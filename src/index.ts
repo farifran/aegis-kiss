d803ab08f68d7c1c44e24989cff8092492108175
Your Name <you@example.com>
2026-08-01 13:21:35 -0300

    aegis: issue#106 task 3/3 TokenBucket
    Aegis-Issue: 106
    Aegis-Task: 3/3
    Aegis-Accept: TokenBucket, obterEstadoBitmask

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,6 @@ export function converterMegabitsEmTerabits(megabits: bigint): bigint {
 export function converterGigabitsEmKilobits(gigabits: bigint): bigint {
   return gigabits / 1024n;
 }
+
+import { TokenBucket, obterEstadoBitmask } from './tokenBucket.js';
+export { TokenBucket, obterEstadoBitmask };
