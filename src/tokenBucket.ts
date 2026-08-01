dcca710b9e7958d3a8c389b511ec64e07de89872
Your Name <you@example.com>
2026-08-01 02:49:45 -0300

    aegis: issue#97 task 2/3 obterEstadoBitmask
    Aegis-Issue: 97
    Aegis-Task: 2/3
    Aegis-Accept: obterEstadoBitmask

--- src/tokenBucket.ts
+++ src/tokenBucket.ts
@@ -1,47 +1,20 @@
 class TokenBucket {
-  private _maxTokens: bigint;
-  private _rateBitsPerMs: bigint;
-  private _tokens: bigint;
-  private _lastUpdate: bigint;
+  tokens: bigint;
+  lastUpdate: bigint;
 
-  constructor(maxBytes: bigint, mbps: number) {
-    this._maxTokens = maxBytes * 8n;
-    this._rateBitsPerMs = BigInt(Math.floor(mbps * 8000));
-    this._tokens = this._maxTokens;
-    this._lastUpdate = BigInt(Date.now());
-  }
-
-  update(): void {
-    const now = BigInt(Date.now());
-    const timeDiff = now - this._lastUpdate;
-    this._lastUpdate = now;
-    let newTokens = this._tokens + timeDiff * this._rateBitsPerMs;
-    if (newTokens > this._maxTokens) {
-      newTokens = this._maxTokens;
-    }
-    this._tokens = newTokens;
-  }
-
-  consume(bits: bigint): boolean {
-    this.update();
-    if (this._tokens >= bits) {
-      this._tokens -= bits;
-      return true;
-    }
-    return false;
-  }
-
-  get tokens(): bigint {
-    return this._tokens;
-  }
-
-  get maxTokens(): bigint {
-    return this._maxTokens;
-  }
-
-  get lastUpdate(): bigint {
-    return this._lastUpdate;
+  constructor(tokens: bigint, lastUpdate: number) {
+    this.tokens = tokens;
+    this.lastUpdate = BigInt(lastUpdate);
   }
 }
 
-export { TokenBucket };
+export function obterEstadoBitmask(bucket: TokenBucket): number {
+  let mask = 0;
+  if (bucket.tokens === 0n) {
+    mask |= 1;
+  }
+  if (BigInt(Date.now()) - bucket.lastUpdate < 1000n) {
+    mask |= 2;
+  }
+  return mask;
+}
