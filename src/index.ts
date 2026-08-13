84425784aab0db60889355a0b325c2ae8969cfb6
Your Name <you@example.com>
2026-08-13 12:40:12 -0300

    aegis: issue#164 converterMegabitsEmTerabits
    Aegis-Issue: 164
    Aegis-Accept: converterMegabitsEmTerabits

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,7 @@
 export function converterKilobitsEmTerabits(kb: number): number {
   return kb / 1e12
 }
+
+export function converterMegabitsEmTerabits(megabits: number): number {
+  return megabits / 1000
+}
