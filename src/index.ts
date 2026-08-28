bef63961810e24b8c6eed31bea2c81a7ffb30db9
Your Name <you@example.com>
2026-08-28 20:43:50 -0300

    aegis: issue#337 task 1/3 ClearinghouseCore
    Aegis-Issue: 337
    Aegis-Task: 1/3
    Aegis-Accept: ClearinghouseCore, obterClearinghouseBitmask

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 // src/index.ts
 import { SettlementBus, obterSaudeBitmask } from './settlementBus.js';
 export { SettlementBus, obterSaudeBitmask };
+
+import { ClearinghouseCore, obterClearinghouseBitmask } from './clearinghouseCore.js';
+export { ClearinghouseCore, obterClearinghouseBitmask };
