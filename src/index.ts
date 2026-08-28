11f44fb0da58b2470681026c84df8e7ff952d8d1
Your Name <you@example.com>
2026-08-28 20:41:01 -0300

    chore: reset workspace baseline for re-testing ClearinghouseCore

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
 // src/index.ts
 import { SettlementBus, obterSaudeBitmask } from './settlementBus.js';
 export { SettlementBus, obterSaudeBitmask };
-
-import { ClearingEngine, obterEstadoCompensacaoBitmask } from './clearingEngine.js';
-export { ClearingEngine, obterEstadoCompensacaoBitmask };
-
-import { ClearinghouseCore, obterClearinghouseBitmask } from './clearinghouseCore.js';
-export { ClearinghouseCore, obterClearinghouseBitmask };
