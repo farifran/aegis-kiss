617b1e4bdf7e017e75d79e47064cf2f97c0fd285
Your Name <you@example.com>
2026-08-28 18:37:46 -0300

    aegis: issue#332 task 1/3 ClearingEngine
    Aegis-Issue: 332
    Aegis-Task: 1/3
    Aegis-Accept: ClearingEngine, obterEstadoCompensacaoBitmask

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 // src/index.ts
 import { SettlementBus, obterSaudeBitmask } from './settlementBus.js';
 export { SettlementBus, obterSaudeBitmask };
+
+import { ClearingEngine, obterEstadoCompensacaoBitmask } from './clearingEngine.js';
+export { ClearingEngine, obterEstadoCompensacaoBitmask };
